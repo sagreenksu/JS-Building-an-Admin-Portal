10add88d7eca01c2b82fdd61b5fe9c04f2c263e6
Scott Green <sagreenxyz@gmail.com>
2022-01-26 12:18:52 -0600

    With onus Delete Functionality

--- public/admin.js
+++ public/admin.js
@@ -16,23 +16,44 @@ async function updateQty(id, quantity) {
     let responseText = await response.json();
 }
 
+async function deleteBook(id) {
+    let response = await fetch(`http://localhost:3001/removeBook/${id}`, {
+        method: "DELETE"
+    });
+    let responseText = await response.json();
+    console.log(responseText);
+}
+
+
 async function listBooks(element) {
     let response = await fetch('http://localhost:3001/listBooks')
     let responseText = await response.json();
     let ul = document.createElement('ul')
-    responseText.forEach((e)=>{
+    responseText.forEach((item)=>{
         let li = document.createElement('li');
-        li.innerText = `${e.title} `
+        li.innerText = `${item.title} `
+        li.setAttribute('id', `list-item-${item.id}`)
+
         let input = document.createElement('input');
-        input.setAttribute('id', e.id);
-        input.setAttribute('value', e.quantity);
+        input.setAttribute('id', item.id);
+        input.setAttribute('value', item.quantity);
         li.append(input)
-        let btn = document.createElement('button');
-        btn.innerText = 'Save';
-        btn.addEventListener('click', () => {
-            updateQty(e.id, input.value)
+
+        let btnSaveItem = document.createElement('button');
+        btnSaveItem.innerText = 'Save';
+        btnSaveItem.addEventListener('click', () => {
+            updateQty(item.id, input.value)
         });
-        li.append(btn);
+        li.append(btnSaveItem);
+
+        let btnDelete = document.createElement('button');
+        btnDelete.innerText = 'Delete';
+        btnDelete.addEventListener('click', () => {
+            deleteBook(item.id)
+            document.getElementById(`list-item-${item.id}`).remove();
+        });
+        li.append(btnDelete);
+
         ul.append(li);
     })
     root.append(ul);
